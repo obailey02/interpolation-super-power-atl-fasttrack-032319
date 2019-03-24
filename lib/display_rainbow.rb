f7fbe59ce90dcbe7b25d121ef45b384ad9391df4
Otis Bailey III <obailey04@yahoo.com>
2019-03-24 15:35:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,8 @@
 display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
 
 display_rainbow "r; red, O: orange, Y: Yellow, G: green, B: blue, I: indigo, V: violet"
+
+letters = ["b","a", "c"]
+puts "the firs letter in the alphabet is {letters [1]}"
+puts "the second letter in the alphabet is {letters[0]}"
+puts "the third letter in the is: {letters[2]}"
